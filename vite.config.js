8d50d4f294d31f58244288669b8a59473451af7c
sangtrandev00 <nhatsang0101@gmail.com>
2022-05-25 06:58:52 +0700

    init commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+const { resolve } = require('path');
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // nested: resolve(__dirname, 'nested/index.html'),
+        postDetail: resolve(__dirname, 'post-detail.html'),
+        addEditPost: resolve(__dirname, 'add-edit-post.html'),
+      },
+    },
+  },
+});
